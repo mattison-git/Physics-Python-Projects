6ae7208b66c1064580c8a391fdfa5b3c99bff1a4
Sasha Mattison <sashastoolbox@gmail.com>
2025-10-22 20:31:09 -0700

    Completed PhysicsProject1.py

--- PhysicsProject1.py
+++ PhysicsProject1.py
@@ -1,44 +1,55 @@
-'''
-Coding task 2: make graphs of velocity versus height for a 50km free-fall
-a) assuming -9.80 m/2 of acceleration
-b) correcting for the variation in 'g' over the 50km altitude
-c) ** including quadratic drag force with a constant coefficient
-d) **including quadratic drag with a 'density scale height' drag co-efficient
-'''
-
 import numpy as np
 import matplotlib.pyplot as plt
 
+e = np.e
 
 height = 50000
 velocity = 0
 dt = 0.1
+
 earth_radius = 6380000  
 constant_G = 6.674*10**-11
 earth_mass = 5.972*10**24
-gravity = constant_G*(earth_mass/(earth_radius+height)**2)
 
-'''
-Equation for gravity is g(h) = G(M/(R+H)^2)
-'''
-def calculate_gravity(height_for_equation):
-     return constant_G*(earth_mass/(earth_radius+height_for_equation)**2)
+def calculate_gravity(h):
+     return constant_G*(earth_mass/(earth_radius+h)**2)
+
+sea_level_air_density = 1.225
+air_density_scale_constant = 8500
+
+def calculate_air_density(h):
+    return sea_level_air_density*e**(-h/air_density_scale_constant)
+    
+drag_coefficient = 1
+cross_sectional_area = 1
+mass = 80 
+
+def calculate_drag_acceleration(v, h, variable_density=True):
+    rho = calculate_air_density(h) if variable_density else sea_level_air_density
+    k = 0.5 * rho * drag_coefficient * cross_sectional_area
+    return k * v**2 / mass
 
 x_values = []
 y_values = []
 
+
+variable_density = True
+
 while height > 0:
     x_values.append(velocity)
     y_values.append(height)
+    
     gravity = calculate_gravity(height)
-    velocity += gravity * dt
+    drag_acc = calculate_drag_acceleration(velocity, height, variable_density)
+    
+    acceleration = gravity - drag_acc
+    
+    velocity += acceleration * dt
     height -= velocity * dt
-
-
 
 plt.plot(x_values, y_values)
 plt.title("Graph of Velocity vs. Height")
 plt.xlabel("Velocity")
 plt.ylabel("Height")
+plt.grid(True)
 plt.show()
-
